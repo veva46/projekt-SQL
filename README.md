9a0d8bb960d98c0df1708a446bae00cc121e69c6
veva46 <tavata@seznam.cz>
2025-07-13 23:32:01 +0200

    formatovani

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Sekundarni tabulka: t_vera_vavrincova_project_sql_secondary_final
 Jednotlive ukoly (vyzkumne otazky) jsou v souborech "V.Vavrincova - ukol 1.sql" az "V.Vavrincova - ukol 5.sql"
 Popis jednotlivych ukolu je uveden v souboru "V.Vavrincova - popis projektu SQL.instructions.md".
 
+Jednotlivé skripty jsou formátovány pomocí https://codebeautify.org/sqlformatter
+
 V jednotlivych ukolech je vzdy uvedeno nekolik dotazu(skriptu) podle zdroje: 
 pro zdroj z tabulek Engeta a pote pro zdroj s primarni (resp. sekundarni) tabulky. 
 U skriptu je kratky popis. Vypocet pres primarni a sekundarni tabulky trva mnohem delsi cas.
--- V.Vavrincova - Sekundarni_tabulka.sql
+++ V.Vavrincova - Sekundarni_tabulka.sql
@@ -3,31 +3,37 @@
 
 --Sekundární tabulka
 
-CREATE TABLE t_vera_vavrincova_project_SQL_secondary_final AS  
+CREATE TABLE t_vera_vavrincova_project_SQL_secondary_final AS 
 SELECT 
-    country,
-    year,
-    population,
-    gini,
-    gdp,
-    tpr.rok_m,
-    tpr.kvartal_m,
-    tpr.prumer_mzda,
-    tpr.odvetvi,
-    tpr.rok_p,
-    tpr.kvartal_p,
-    tpr.prumer_cena_potr,
-    tpr.category_code,
-    tpr.potravina,
-    tpr.jednotka
-FROM economies e 
-   JOIN t_vera_vavrincova_project_sql_primary_final tpr
-      ON e.year = tpr.rok_m
-      WHERE tpr.kvartal_m = tpr.kvartal_p 
-      AND tpr.rok_m = tpr.rok_p
-      AND e.country LIKE '%Czech%'   --pro Českou republiku
-   ORDER BY tpr.rok_m, tpr.kvartal_m, country, odvetvi, potravina, tpr.prumer_cena_potr   
-; 
+  country, 
+  year, 
+  population, 
+  gini, 
+  gdp, 
+  tpr.rok_m, 
+  tpr.kvartal_m, 
+  tpr.prumer_mzda, 
+  tpr.odvetvi, 
+  tpr.rok_p, 
+  tpr.kvartal_p, 
+  tpr.prumer_cena_potr, 
+  tpr.category_code, 
+  tpr.potravina, 
+  tpr.jednotka 
+FROM 
+  economies e 
+  JOIN t_vera_vavrincova_project_sql_primary_final tpr ON e.year = tpr.rok_m 
+WHERE 
+  tpr.kvartal_m = tpr.kvartal_p 
+  AND tpr.rok_m = tpr.rok_p 
+  AND e.country LIKE '%Czech%' --pro Českou republiku
+ORDER BY 
+  tpr.rok_m, 
+  tpr.kvartal_m, 
+  country, 
+  odvetvi, 
+  potravina, 
+  tpr.prumer_cena_potr;
 
 
 SELECT * FROM t_vera_vavrincova_project_SQL_secondary_final;
